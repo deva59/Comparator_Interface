b20f690a325553b84bcaa619b120760cc0232158
deva59 <devendrashimpi2001@gmail.com>
2024-12-19 17:53:37 +0530

    Comparator_Interface

--- UsingQueue.java
+++ UsingQueue.java
@@ -1,23 +1,29 @@
 package Comparator_Interface;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.*;
 
 class UsingQueue{
     public static void main(String[] args) {
         Comparator<Integer> ff = new Comparator<Integer>() {
             @Override
             public int compare(Integer i, Integer j) {
-                if (i > j)
+                if (i%10 > j%10)
                     return 1;
                 else
                     return -1;
             }
+
         };
 
-        List<Integer> dd = new ArrayList<>(){
-            dd.add(1);
+        List<Integer> dd = new ArrayList<>();
+            dd.add(17);
+            dd.add(21);
+            dd.add(39);
+            dd.add(43);
 
+         Collections.sort(dd, ff);
+        System.out.println(dd);
     }
 }
